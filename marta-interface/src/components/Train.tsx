4550346aa52cd32987894cad8d2b952c07c543f9
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2026-03-01 02:39:14 -0500

    structure

---
+++ marta-interface/src/components/Train.tsx
@@ -0,0 +1,2 @@
+// exportable train component that takes in data for one train and returns the display for it
+// check if DELAY === "T0S"
---
+++ marta-interface/src/pages/TrainList.tsx
@@ -0,0 +1,12 @@
+export default function LinesPage() {
+  // initialize some currColor state
+
+  return (
+    <div>
+      // YOUR JSX CODE
+      <NavBar color={currColor} data={stationData} />
+      <TrainList color={currColor} data={trainData} />
+      // YOUR JSX CODE
+    </div>
+  );
+}
